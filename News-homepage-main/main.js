bade95239b19163ea0aa28f97698e53a6a9b9021
omargabrielaguilar <saucyfunne@gmail.com>
2023-01-19 10:49:12 -0500

    listo :D

--- News-homepage-main/main.js
+++ News-homepage-main/main.js
@@ -1,1 +1,12 @@
-console.log("hola")
+const body = document.querySelector(".body");
+const mobileNav = document.querySelector(".mobile-nav") 
+const comolomueveelbody = document.querySelector(".tema")
+
+function openModal() {
+  mobileNav.classList.toggle("navList-mobile");
+  
+}
+
+function closeModal() {
+  mobileNav.classList.toggle("navList-mobile");
+}
